d7257ec2f9202f9f1d490120f8170202849ca915
Shubham Kolar <82713328+Shubhamkolar@users.noreply.github.com>
2023-10-04 19:31:04 +0000

    OperatorOverloading and DAY3 commit

---
+++ DAY3/OperatorOverloading/Vehicle.h
@@ -0,0 +1,56 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <iostream>
+#include "VehicleType.h"
+class Vehicle
+{
+private:
+    std::string _id;
+    std::string _brand;
+    float _price;
+    VehicleType _type;
+
+public:
+    // Vehicle() = delete; // disabled default constructor
+    Vehicle(std::string id, std::string brand, float price, VehicleType type);
+    Vehicle(const Vehicle &) = default;
+    Vehicle(/* args */)
+    {
+        std::cout
+            << "Vehicle with ID :"
+            << _id
+            << " is destroyed\n";
+    }
+    float CalculateTax();
+    float CalculateTax(float tax_percent);
+    float CalculateTax(float sgst, float cgst);
+    ~Vehicle() {}
+
+    std::string id() const { return _id; }
+
+    std::string brand() const { return _brand; }
+
+    float getPrice() const { return _price; }
+
+    VehicleType type() const { return _type; }
+
+    friend std::ostream &operator<<(std::ostream &os, const Vehicle &rhs);
+    // friend std::ostream& operator<<(std::ostream& os,Vehicle& obj){
+    //     os<<"ID : "<<obj._id<<"\t"<<"Brand : "<<obj._brand<<"\t"<<"Price : "<<obj._price<<"\t"<<"Type : "<<DisplayEnum(obj._type);
+    //     return os
+    // };
+
+    // e1+e2
+    // hello world
+    // OPERATOR OVERLOADING
+    float operator+(Vehicle e2)
+    {
+        // Current objects price + past objects price
+        return this->getPrice() + e2.getPrice();
+    }
+};
+
+std::string DisplayEnum(const VehicleType value);
+
+#endif // VEHICLE_H
